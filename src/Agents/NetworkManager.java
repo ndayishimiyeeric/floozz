a44883d7bffcb30f5b5ca56f876f1dd0dfa0bce8
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2024-02-29 02:54:41 +0100

    add mail boax to network manager

--- src/Agents/NetworkManager.java
+++ src/Agents/NetworkManager.java
@@ -1,6 +1,18 @@
 package Agents;
 
+import Utils.MailBox;
+import jade.core.AID;
 import jade.core.Agent;
 
+
 public class NetworkManager extends Agent {
+    private final MailBox mailBox;
+
+    public NetworkManager(MailBox mailBox) {
+        this.mailBox = new MailBox(this.getAID());
+    }
+
+    public void receiveMessage(String content, AID senderId) {
+        mailBox.receiveMessage(content, senderId);
+    }
 }
