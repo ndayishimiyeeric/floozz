5123cd4b74d30806e4260d29183e190e2f4e3d5e
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2024-02-29 01:58:02 +0100

    add jade

---
+++ src/Agents/Consumer.java
@@ -0,0 +1,6 @@
+package Agents;
+
+import jade.core.Agent;
+
+public class Consumer extends Agent {
+}
---
+++ src/Agents/Producer.java
@@ -0,0 +1,6 @@
+package Agents;
+
+import jade.core.Agent;
+
+public class Producer extends Agent {
+}
