26b66b2ae673235583fd4b110f3d276c503fc2a4
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2024-03-02 03:45:10 +0100

    fix compiler errors

--- src/Behaviors/RespondToRequestBehavior.java
+++ src/Behaviors/RespondToRequestBehavior.java
@@ -1,5 +1,19 @@
-public class RespondToRequestBehavior extends Behavior {
+package Behaviors;
+
+import jade.core.behaviours.Behaviour;
+
+public class RespondToRequestBehavior extends Behaviour {
 
     public RespondToRequestBehavior() {
+    }
+
+    @Override
+    public void action() {
+
+    }
+
+    @Override
+    public boolean done() {
+        return false;
     }
 }
