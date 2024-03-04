594696b4a2df30e92ad99141cdc4a4509d00b1a3
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2024-03-04 01:28:29 +0100

    Implement Handle energy request service behaviour

--- src/Utils/EnergySerialization.java
+++ src/Utils/EnergySerialization.java
@@ -1,10 +1,18 @@
 package Utils;
 
 import com.google.gson.Gson;
+import jade.core.AID;
+
+import java.util.Map;
 
 public class EnergySerialization {
     public static String serializeToJson(Energy energy) {
         Gson gson = new Gson();
         return gson.toJson(energy);
+    }
+
+    public static String serializeToJson(Map<AID, Energy> available) {
+        Gson gson = new Gson();
+        return gson.toJson(available);
     }
 }
